0d2e92686e42eafb1d702276b2fda60fb4557585
itamarnita <99781918+itamarnita@users.noreply.github.com>
2022-02-22 17:35:56 +0800

    Add files via upload

---
+++ day4_stdin_and_stdout_II_java.java
@@ -0,0 +1,24 @@
+
+package Ita_Marnita;
+import java.util.Scanner;
+
+public class day4_stdin_and_stdout_II_java {
+    public static void main(String[] args) {
+        int i;
+        double d;
+        String s;
+        try (Scanner scan = new Scanner(System.in)) {
+            i = scan.nextInt();
+            d = scan.nextDouble();
+            scan.nextLine();
+            s = scan.nextLine();
+        }
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+    
+}

b3b960b996e43c4b003689e0252aea914b35402f
itamarnita <99781918+itamarnita@users.noreply.github.com>
2022-03-11 18:30:19 +0800

    Add files via upload

---
+++ day21_java_Regular_Expression.java
@@ -0,0 +1,17 @@
+
+package Ita_Marnita;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+        
+
+
+public class day21_java_Regular_Expression {
+    public static void main(String[] args) {
+     //Write your code here
+class MyRegex{
+    public String pattern = "((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\b)";
+}   
+    }
+    
+}
